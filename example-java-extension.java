c8828f6ab266ecc8deac7d091042f749c35bf586
mostypc123 <140317448+mostypc123@users.noreply.github.com>
2023-07-27 13:41:27 +0200

    Create example-java-extension.java

---
+++ example-java-extension.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class FactorialCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number to calculate its factorial: ");
+        int n = scanner.nextInt();
+        
+        if (n < 0) {
+            System.out.println("Factorial cannot be calculated for negative numbers.");
+        } else {
+            long factorial = 1;
+            for (int i = 1; i <= n; i++) {
+                factorial *= i;
+            }
+            System.out.println("Factorial of " + n + " is: " + factorial);
+        }
+        scanner.close();
+    }
+}
