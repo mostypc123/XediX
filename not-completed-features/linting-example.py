1b158d08bf4788e4ae541275323abc3cde558ba3
mostypc123 <140317448+mostypc123@users.noreply.github.com>
2023-07-28 11:52:58 +0200

    Create linting-example.py

---
+++ not-completed-features/linting-example.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+from pylint import epylint as lint
+from tkinter import messagebox
+
+def save_text():
+    with open("my_code.py", "w") as file:
+        code = text_box.get("1.0", "end-1c")
+        file.write(code)
+
+def check_code():
+    code = text_box.get("1.0", "end-1c")
+    (pylint_stdout, pylint_stderr) = lint.py_run(code, return_std=True)
+
+    # Zobrazenie chýb v Textboxe
+    output = pylint_stdout.getvalue()
+    messagebox.showerror("errors",output)
+    
+
+root = tk.Tk()
+root.title("Text Editor")
+
+text_box = tk.Text(root, wrap="word")
+text_box.pack(expand=True, fill="both")
+
+save_button = tk.Button(root, text="Save", command=save_text)
+save_button.pack()
+
+check_button = tk.Button(root, text="Check Code", command=check_code)
+check_button.pack()
+
+root.mainloop()
