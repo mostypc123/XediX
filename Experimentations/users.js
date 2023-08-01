88c4170281abd745e8e99c1a9a1494183c15fca9
mostypc123 <140317448+mostypc123@users.noreply.github.com>
2023-08-01 11:27:42 +0200

    Update users.js. This is only EXPERIMENT

--- Experimentations/users.js
+++ Experimentations/users.js
@@ -1,67 +1,54 @@
-// Import balíčkov
-const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
-const bcrypt = require('bcrypt');
+// Nainštalujte balíčky pre Node.js
+// npm install firebase-admin express
 
-// Inicializácia Express aplikácie
+const express = require('express');
+const admin = require('firebase-admin');
+
+// Inicializácia aplikácie s Firebase konfiguráciou
+const serviceAccount = require('./path/to/serviceAccountKey.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://your-firebase-project.firebaseio.com'
+});
+
 const app = express();
 app.use(express.json());
-
-// Vytvorenie pripojenia k SQLite3 databáze
-const db = new sqlite3.Database('users.db');
 
 // Cesta na registráciu nového používateľa
 app.post('/register', async (req, res) => {
   try {
     const { username, email, password } = req.body;
-    // Hashovanie hesla
-    const hashedPassword = await bcrypt.hash(password, 10);
+    // Vytvorenie nového používateľa s e-mailom a heslom
+    await admin.auth().createUser({
+      email: email,
+      password: password
+    });
 
-    // Uloženie používateľa do databázy
-    db.run(
-      'INSERT INTO users (username, email, password) VALUES (?, ?, ?)',
-      [username, email, hashedPassword],
-      (err) => {
-        if (err) {
-          console.error(err.message);
-          return res.status(500).json({ error: 'Registrácia zlyhala.' });
-        }
-        return res.status(201).json({ message: 'Registrácia prebehla úspešne.' });
-      }
-    );
+    // Vytvorenie nového záznamu v databáze s ďalšími údajmi o používateľovi
+    await admin.database().ref('users').push({
+      username: username,
+      email: email
+    });
+
+    return res.status(201).json({ message: 'Registrácia prebehla úspešne.' });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: 'Nastala chyba.' });
   }
 });
 
-// Cesta na prihlásenie používateľa
+// Cesta na prihlásenie používateľa (nemáme potrebu hashovať heslo, Firebase to spraví za nás)
 app.post('/login', async (req, res) => {
   try {
-    const { username, password } = req.body;
+    const { email, password } = req.body;
 
-    // Získanie uloženého hesla z databázy na základe používateľského mena
-    db.get('SELECT password FROM users WHERE username = ?', [username], async (err, row) => {
-      if (err) {
-        console.error(err.message);
-        return res.status(500).json({ error: 'Prihlásenie zlyhalo.' });
-      }
+    // Prihlásenie používateľa pomocou e-mailu a hesla
+    const user = await admin.auth().signInWithEmailAndPassword(email, password);
 
-      // Porovnanie zadaného hesla s uloženým hashom
-      if (!row) {
-        return res.status(404).json({ error: 'Používateľ neexistuje.' });
-      }
-      const passwordMatch = await bcrypt.compare(password, row.password);
-
-      if (!passwordMatch) {
-        return res.status(401).json({ error: 'Nesprávne heslo.' });
-      }
-
-      return res.json({ message: 'Prihlásenie prebehlo úspešne.' });
-    });
+    return res.json({ message: 'Prihlásenie prebehlo úspešne.' });
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ error: 'Nastala chyba.' });
+    return res.status(500).json({ error: 'Prihlásenie zlyhalo.' });
   }
 });
 
@@ -69,3 +56,4 @@ app.post('/login', async (req, res) => {
 app.listen(3000, () => {
   console.log('Server spustený na porte 3000');
 });
+
