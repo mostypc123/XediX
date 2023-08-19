e557e01a5cb2f26dd861cf5469ea4044510f2c0a
Juraj K <140317448+mostypc123@users.noreply.github.com>
2023-08-19 08:43:20 +0200

    Update and rename linting-example.py to test_linting-example.py

--- not-completed-features/Python/test_linting-example.py
+++ not-completed-features/Python/test_linting-example.py
@@ -2,12 +2,12 @@ import tkinter as tk
 from pylint import epylint as lint
 from tkinter import messagebox
 
-def save_text():
+def test_save_text():
     with open("my_code.py", "w") as file:
         code = text_box.get("1.0", "end-1c")
         file.write(code)
 
-def check_code():
+def test_check_code():
     code = text_box.get("1.0", "end-1c")
     (pylint_stdout, pylint_stderr) = lint.py_run(code, return_std=True)
 
@@ -22,10 +22,10 @@ root.title("Text Editor")
 text_box = tk.Text(root, wrap="word")
 text_box.pack(expand=True, fill="both")
 
-save_button = tk.Button(root, text="Save", command=save_text)
+save_button = tk.Button(root, text="Save", command=test_save_text)
 save_button.pack()
 
-check_button = tk.Button(root, text="Check Code", command=check_code)
+check_button = tk.Button(root, text="Check Code", command=test_check_code)
 check_button.pack()
 
 root.mainloop()
